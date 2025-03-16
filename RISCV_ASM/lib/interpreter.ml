fbc7bdc80ff2049d76e811bdc031157b3188bdc0
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-03-16 17:32:53 +0300

    test: imporve factorial cram test
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- RISCV_ASM/lib/interpreter.ml
+++ RISCV_ASM/lib/interpreter.ml
@@ -546,7 +546,7 @@ let execute_load_int rd rs1 imm size is_signed =
 ;;
 
 let execute_store_int rs1 rs2 imm size =
-  let* base_address = get_register_value rs1 in
+  let* base_address = get_register_value rs2 in
   let* address_info = get_address12_value imm in
   let offset =
     match address_info with
@@ -554,7 +554,7 @@ let execute_store_int rs1 rs2 imm size =
     | LabelAddress label_address -> label_address
   in
   let address = Int64.add base_address (sext offset) in
-  let* value = get_register_value rs2 in
+  let* value = get_register_value rs1 in
   store_in_memory address value size
 ;;
 
@@ -954,8 +954,6 @@ let traverse_program () =
     match expr_opt with
     | None -> return state
     | Some (InstructionExpr instr) ->
-      (*let () = pp_instruction Format.std_formatter instr in
-        let () = print_string (show_state state) in*)
       let* () = execute_instruction instr in
       let* () = increment_program_idx () in
       prog_trav_helper ()
